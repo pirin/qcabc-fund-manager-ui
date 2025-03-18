96d8bae89410ab7f04de109614aca2d77e6c9bad
Stefan Kyntchev <stefan@cnbssoftware.com>
2025-03-18 06:59:31 -0400

    added timestamp param to the /portfolio endpoint

--- packages/nextjs/app/api/portfolio/route.ts
+++ packages/nextjs/app/api/portfolio/route.ts
@@ -1,12 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export const revalidate = 0;
+
+export async function GET(req: NextRequest) {
   const min = 10000;
   const max = 100000;
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
+  const timestamp = req.nextUrl.searchParams.get("ts") || Date.now();
+
   return NextResponse.json(
-    { portfolioValue: randomNumber * 1000000 },
+    { portfolioValue: randomNumber * 1000000, timestamp: timestamp },
+
     {
       headers: {
         "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
